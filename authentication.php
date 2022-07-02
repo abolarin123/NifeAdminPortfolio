0e40a6b59fd779324a94d1591f1660856ae6a07a
abolarin babatunde <abolarinbabatunde88@gmail.com>
2022-07-02 10:40:05 -0700

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -8,9 +8,9 @@ $password =isValidated($_POST['password']);
 $password= md5($password);
 //print $password;
 if($check==true){
-   $sql ='SELECT COUNT(*)FROM `tbl_users` WHERE email ="$email"';
-$result = mysqli_query($con,$sql);
-   echo $result ;
+    $sql ='SELECT * FROM `tbl_users` WHERE email ="$email"';
+    $result = mysqli_query($con,$sql);
+   echo mysqli_num_rows($result);
 }else{
     $_SESSION['error'] = "The data is incorrect";
     header("Location: ../views/auth/register.php ");
