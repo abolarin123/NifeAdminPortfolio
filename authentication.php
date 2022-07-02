db0875b38dfcbf98c29c82e85f664eb094c64377
NifayAjayi <63299256+NifayAjayi@users.noreply.github.com>
2022-07-02 13:34:27 -0400

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -1,16 +1,19 @@
 <?php 
 //print($_POST['username']);
 include('validation.php');
+include('connDriver.php');
 $username =isValidated($_POST['username']);
 $email=isValidated(isValidEmail($_POST['email']));
 $password =isValidated($_POST['password']);
 $password= md5($password);
 //print $password;
 if($check==true){
-   
+   $sql ='SELECT COUNT(*)FROM `tbl_users` WHERE email ="$email"';
+$result = mysqli_query($con,$sql);
+   echo $result ;
 }else{
-    $_SESSION['invalid'] = "the data is incorrect";
+    $_SESSION['error'] = "The data is incorrect";
     header("Location: ../views/auth/register.php ");
-    
+
 }
 ?>
