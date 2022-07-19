901025030afc705f306fa0af2eb52ec7894d69e8
NifayAjayi <63299256+NifayAjayi@users.noreply.github.com>
2022-07-19 13:16:40 -0400

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -13,17 +13,36 @@ if($check==true){
   // echo mysqli_num_rows($result);
    if (mysqli_num_rows($result)>0) {
     //print true;
-    $sql ="INSERT INTO `tbl_users`( `username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
-    $result = mysqli_query($con, $sql);
+    $_SESSION['error'] = "Email already in use";
+    header("Location: ../views/auth/register.php ");
+   
    } else {
     # code...
-    $_SESSION['record'] = "Email already exists";
-    header("Location: ../views/auth/register.php ");
+  
+    $sql ="INSERT INTO `tbl_users`( `username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+    $result = mysqli_query($con, $sql);
+    $user_id=mysqli_insert_id($con);
+   // print $user_id;
+    if(isset($user_id)) {
+       auth($user_id,$username,$email);
+    }
+
+    
+
    }
    
 }else{
     $_SESSION['error'] = "The data is incorrect";
     header("Location: ../views/auth/register.php ");
+
+    } 
+    function auth($userID,$Username,$userEmail)
+{
+  $_SESSION['userid'] = $userID;
+  $_SESSION['username']= $username;
+  $_SESSION['useremail']= $userEmail;
+  $_SESSION['last_login_time']= time();
+  header("Location: ../views/pages/dashboard.php ");
 
 }
 ?>
